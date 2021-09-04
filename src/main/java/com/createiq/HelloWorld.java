4d3eeb2421c710c8b1cacdb899eb96ca9c4d07bc
obulareddy665 <obulareddy665@example.com>
2021-09-04 22:10:00 +0530

    First Coomit

---
+++ src/main/java/com/createiq/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.createiq;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorld {
+	@RequestMapping("/hello")
+	public String hello() {
+		return "helloworld";
+	}
+	@RequestMapping("/welcome")
+	public String welcome() {
+		return "welcome";
+	}
+}
